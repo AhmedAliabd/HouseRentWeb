321727e763a021d795e826033b09acdd7c6e1bd3
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2022-01-04 03:25:24 -0500

    added editPage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ function Navbar() {
                   : "navbarListItemName"
               }
             >
-              Offer
+              Offers
             </p>
           </li>
           <li onClick={() => navigate("/profile")} className="navbarListItem">
